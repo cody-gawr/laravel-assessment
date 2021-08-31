17d5a0a0d3dd04e3c187520040f1137b1c0f8875
yhy940806 <yhy940806@163.com>
2021-08-31 02:25:08 -0400

    – add some constants

--- config/constants.php
+++ config/constants.php
@@ -1,1 +1,26 @@
 <?php
+
+return [
+    "achievements" => [
+        "lessons" => [
+            "First Lesson Watched",
+            "5 Lessons Watched",
+            "10 Lessons Watched",
+            "25 Lessons Watched",
+            "50 Lessons Watched"
+        ],
+        "comments" => [
+            "First Comment Written",
+            "3 Comments Written",
+            "5 Comments Written",
+            "10 Comment Written",
+            "20 Comment Written"
+        ],
+        "badges" => [
+            ["achievement_count" => 0, "badge" => "Beginner"],
+            ["achievement_count" => 4, "badge" => "Intermediate"],
+            ["achievement_count" => 8, "badge" => "Advanced"],
+            ["achievement_count" => 10, "badge" => "Master"]
+        ]
+    ]
+];
