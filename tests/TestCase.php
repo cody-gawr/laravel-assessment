e5781f772ea3141a22e529018eefbad4d51595bd
yhy940806 <yhy940806@163.com>
2021-09-01 11:32:17 -0400

    – do some configurations for Unit Test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (!isset($_ENV["SETUP_DB"])) {
+            $this->artisan("migrate:fresh");
+            $this->artisan("db:seed");
+
+            $_ENV["SETUP_DB"] = true;
+        }
+    }
 }
---
+++ tests/Unit/AchieveServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class AchieveServiceTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
